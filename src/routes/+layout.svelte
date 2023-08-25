5b67820257bb7646ffafaf13a60088e95f4e5a03
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-08-25 17:21:57 -0400

    removed prerender...

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+
+<script lang="ts" context="module">
+  import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+    <meta name="description" content={$page.url.hostname === "iamdefinitelylearning.vercel.app" ? 
+        "I am definitely learning bro trust me" :
+        "Ghost and Cakes 3D - a game made with $317 billion budget, powering your mom's credit card since 2018 BC"
+    } />
+</svelte:head>
+
+<slot />
