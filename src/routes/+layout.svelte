49835bad51fbc861763ee87472a70bb4818a7c18
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-08-31 20:07:15 -0600

    push test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,21 @@
-
 <script lang="ts">
-  import { page } from "$app/stores";
-  // console.log($page.url.hostname === "iamdefinitelylearning.vercel.app");
+	import { page } from "$app/stores";
+	import "../app.css";
+	// console.log($page.url.hostname === "iamdefinitelylearning.vercel.app");
 </script>
 
 <svelte:head>
-    <meta name="description" content={$page.url.hostname === "iamdefinitelylearning.vercel.app" ? 
-        "I am definitely learning bro trust me" :
-        "Ghost and Cakes 3D - a game made with $317 billion budget, powering your mom's credit card since 2018 BC"
-    } />
-    <title>{$page.url.hostname === "iamdefinitelylearning.vercel.app" ? "I AM LEARNING TRUST!1!" : "GNC3D - Powering your mom's credit card since 2018 BC"}</title>
+	<meta
+		name="description"
+		content={$page.url.hostname === "iamdefinitelylearning.vercel.app"
+			? "I am definitely learning bro trust me"
+			: "Ghost and Cakes 3D - a game made with $317 billion budget, powering your mom's credit card since 2018 BC"}
+	/>
+	<title
+		>{$page.url.hostname === "iamdefinitelylearning.vercel.app"
+			? "I AM LEARNING TRUST!1!"
+			: "GNC3D - Powering your mom's credit card since 2018 BC"}</title
+	>
 </svelte:head>
 
 <slot />
