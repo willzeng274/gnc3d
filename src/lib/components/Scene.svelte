91eecd80b4f185f163f12a9d28c44e9cff9fd46a
Rashaad1268 <74459673+Rashaad1268@users.noreply.github.com>
2023-09-03 03:58:21 +0000

    Fix alignment issue

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -546,13 +546,14 @@
 		</Root>
 		<Assets />
 		<Root>
-			<section class="w-full h-[20%] lg:h-full flex flex-col lg:flex-row items-center justify-end" class:hide={isSuspend}>
-				<ul class="flex flex-row lg:flex-col h-full items-center justify-center mr-1 w-full lg:w-[15%] bg-slate-800 text-white list-none rounded-md p-1 z-[1]">
+			<section class="w-full h-full flex flex-col lg:flex-row items-center justify-end" class:hide={isSuspend}>
+				<ul class="flex flex-row lg:flex-col h-[20%] lg:h-full items-center justify-center mr-1 w-full lg:w-[15%] bg-slate-800 text-white list-none rounded-md p-1 z-[1]">
 					{#each contextMenuItems as ctx}
 						<li
 							data-state={currentCtx == ctx ? "active" : ""}
-							class="flex flex-col items-center justify-center w-[90%] lg:h-[7%] p-4
-								duration-500 hover:h-[10%] border border-solid border-slate-700
+							class="flex flex-col items-center justify-center w-[90%] lg:h-[7%] p-4 transition-[height]
+								duration-500 hover:h-[75%] lg:hover:h-[10%] border border-solid border-slate-700
+								first:rounded-l-[4px] last:rounded-r-[4px]
 								lg:first:rounded-t-[4px] lg:last:rounded-b-[4px]"
 						>
 							<button class="bg-transparent border-none text-lg w-full h-full text-center text-white" on:click={() => (currentCtx = ctx)}>{ctx.name}</button>
