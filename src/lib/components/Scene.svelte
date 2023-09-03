53cb9dc9bea45069d3e4e9927bc96b53760469de
Rashaad1268 <74459673+Rashaad1268@users.noreply.github.com>
2023-09-03 03:06:07 +0000

    style: Made the menu look better

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -546,15 +546,16 @@
 		</Root>
 		<Assets />
 		<Root>
-			<section class="w-full h-full flex flex-row items-center justify-end" class:hide={isSuspend}>
-				<ul class="flex flex-col h-full items-center justify-center mr-1 w-[10%] text-white list-none rounded-md p-1 z-[1]">
+			<section class="w-full h-[20%] lg:h-full flex flex-col lg:flex-row items-center justify-end" class:hide={isSuspend}>
+				<ul class="flex flex-row lg:flex-col h-full items-center justify-center mr-1 w-full lg:w-[15%] bg-slate-800 text-white list-none rounded-md p-1 z-[1]">
 					{#each contextMenuItems as ctx}
 						<li
 							data-state={currentCtx == ctx ? "active" : ""}
-							class="flex flex-col items-center justify-center w-[90%] h-[5%] p-4
-								duration-500 hover:h-[10%] border border-solid border-red-950"
+							class="flex flex-col items-center justify-center w-[90%] lg:h-[7%] p-4
+								duration-500 hover:h-[10%] border border-solid border-slate-700
+								lg:first:rounded-t-[4px] lg:last:rounded-b-[4px]"
 						>
-							<button class="bg-transparent border-none w-full h-full text-center text-white" on:click={() => (currentCtx = ctx)}>{ctx.name}</button>
+							<button class="bg-transparent border-none text-lg w-full h-full text-center text-white" on:click={() => (currentCtx = ctx)}>{ctx.name}</button>
 						</li>
 					{/each}
 				</ul>
