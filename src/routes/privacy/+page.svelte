7262137d3b082448bae7e6110d8dfe2dddb6d6e8
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-09-10 19:40:05 -0400

    added privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,14 @@
+<section class="m-2 font-['Comic_Sans_MS']">
+    <h1 class="text-xl">Privacy policy</h1>
+
+    <h3 class="text-lg">Information we collect:</h3>
+
+    <ol class="ml-8 list-decimal text-md">
+        <li>Locale</li>
+        <li>Remote Address (only for unique visitor validation purposes)</li>
+        <li>Browser</li>
+        <li>Operating System</li>
+        <li>Device</li>
+        <li>Referrer</li>
+    </ol>
+</section>
