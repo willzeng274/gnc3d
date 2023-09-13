3855f620a686ec42307cd8b6f52d65c7bd109fe0
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-09-13 16:37:32 -0400

    updated privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -4,14 +4,15 @@
     <h3 class="text-lg">Information we collect:</h3>
 
     <ol class="ml-8 list-decimal text-md">
-        <li>Locale</li>
+        <li>Locale (e.g. USA, Canada. Used to track player base)</li>
         <li>Remote Address (only for unique visitor validation purposes)</li>
-        <li>Browser</li>
-        <li>Operating System</li>
-        <li>Device</li>
-        <li>Referrer</li>
+        <li>Browser (e.g. Chrome, Edge, Firefox. Used for analytics to track the player base and future supportage)</li>
+        <li>Operating System (e.g. mac, windows, linux. Used for analytics to track the player base)</li>
+        <li>Referrer (if you clicked the link on another website)</li>
     </ol>
 
     <br />
     <p>ALL INFORMATION IS KEPT FOR A MAXIMUM OF 30 DAYS</p>
+    <br />
+    <p>Using vercel analytics</p>
 </section>
